e6810973faee88c9dd7da0ffb4842f6f693d44ed
catmanl <laurentinoluna06@protonmail.com>
2023-07-29 23:41:27 -0300

    improve check.sh readability

--- check.sh
+++ check.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/sh
 problem_file=$1
-if [[ -f $problem_file ]]; then
-    result=$(head -n 1 $problem_file | grep -E -o '[0-9.]+') # result is at first line
-    ext="${problem_file##*.}"
-    output=""
-    if [[ $ext = 'c' ]]; then
-        gcc $problem_file -lm -lgmp -o out
-        output=$(./out)
-        rm out
-    elif [[ $ext = 'py' ]]; then
-        output=$(python $problem_file)
-    else
-        echo "File extension not recognized"
-    fi
+if [[ ! -f $problem_file ]]; then
+    echo "${problem_file} does not exist"
+    exit 0
+fi
 
-    if [[ $output -eq $result ]]; then
-        echo "${output} is correct."
-    else
-        echo "${output} is incorrect. Expected: ${result}"
-    fi
+result=$(head -n 1 $problem_file | grep -E -o '[0-9.]+')
+ext="${problem_file##*.}"
+
+if [[ $ext != 'c' ]]; then # non-c solutions are not allowed!
+    echo "File extension not recognized"
+    exit 0
+fi
+
+gcc $problem_file -lm -lgmp -o out
+output=$(./out)
+rm out
+
+if [[ $output -eq $result ]]; then
+    echo "${output} is correct."
 else
-    echo $problem_file 'does not exist'
+    echo "${output} is incorrect. Expected: ${result}"
 fi
