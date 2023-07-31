e80ab7975c48f8daaeb38ff7075d64e6b9782e04
catmanl <laurentinoluna06@protonmail.com>
2023-07-31 20:30:20 -0300

    clarify where the calculation comes from

--- 028.c
+++ 028.c
@@ -5,13 +5,14 @@
 
 int main()
 {
+    // From pen and paper deduce this summation
+    // This can be done without a for loop... too bad!
     int sum = 0;
-
     for (int i = 1; i <= 500; i++) {
         sum += 4*square(2*i + 1) - 12*i;
     }
 
-    sum++;
+    sum++; // Add the 1 in the center
     printf("%d\n", sum);
 
     return 0;
