770c2c499546447c14ea4490a7f6659c4718882d
lauchimoon <laurentino.luna06@gmail.com>
2024-02-20 12:17:59 -0300

    Redo/Improve 012

--- 012.zig
+++ 012.zig
@@ -1,25 +1,18 @@
 const std = @import("std");
 const stdout = std.io.getStdOut().writer();
+const common = @import("common");
 
-fn solve(divs: u64) u64 {
-    var sum: u64 = 0;
-    var cnt: u64 = 1;
+fn tri(n: u64) u64 {
+    return n*(n + 1)/2;
+}
 
-    while (true) {
-        sum += cnt;
-        cnt += 1;
-
-        var ndivs: i32 = 0;
-        var i: u64 = 1;
-        while (i*i <= sum) {
-            if (@mod(sum, i) == 0) {
-                ndivs += 2;
-            }
-            i += 1;
-        }
-
-        if (ndivs >= divs) {
-            return sum;
+fn solve(n_divs: u64) !u64 {
+    var i: usize = 0;
+    while (true) : (i += 1) {
+        const tri_i = tri(i);
+        const divs = try common.nDivs(tri_i);
+        if (divs > n_divs) {
+            return tri_i;
         }
     }
 
@@ -27,5 +20,6 @@ fn solve(divs: u64) u64 {
 }
 
 pub fn main() !void {
-    try stdout.print("{}\n", .{solve(500)});
+    const solution = try solve(500);
+    try stdout.print("{}\n", .{solution});
 }
