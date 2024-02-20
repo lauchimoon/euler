81eba29ed5bdabf66258ac39a6a7460c345e7bc5
lauchimoon <laurentino.luna06@gmail.com>
2024-02-20 11:15:24 -0300

    Improve 003 solution

--- 003.zig
+++ 003.zig
@@ -1,16 +1,6 @@
 const std = @import("std");
 const stdout = std.io.getStdOut().writer();
 
-fn is_prime(n: u64) bool {
-    for (2..n) |i| {
-        if (@mod(n, i) == 0) {
-            return false;
-        }
-    }
-
-    return true;
-}
-
 fn max_list(list: std.ArrayList(u64)) u64 {
     var m: u64 = 0;
     for (list.items) |item| {
@@ -20,25 +10,31 @@ fn max_list(list: std.ArrayList(u64)) u64 {
     return m;
 }
 
-fn solve(n: u64) anyerror!u64 {
-    const allocator = std.heap.page_allocator;
-    var factors = std.ArrayList(u64).init(allocator);
-    defer factors.deinit();
-
-    for (2..n - 1) |i| {
-        if (i > 1000000) {
-            break;
-        }
-        if (@mod(n, i) == 0 and is_prime(i)) {
-            try factors.append(i);
+fn minDiv(n: u64) u64 {
+    var min: u64 = 2;
+    while (true) : (min += 1) {
+        if (@mod(n, min) == 0) {
+            return min;
         }
     }
+}
 
-    const max = max_list(factors);
-    return max;
+fn solve(n: u64) !u64 {
+    const allocator = std.heap.page_allocator;
+    var arr = std.ArrayList(u64).init(allocator);
+    defer arr.deinit();
+    var n0: u64 = n;
+
+    while (n0 > 1) {
+        var m = minDiv(n0);
+        n0 /= m;
+        try arr.append(m);
+    }
+
+    return max_list(arr);
 }
 
 pub fn main() !void {
-    const max: anyerror!u64 = solve(600851475143);
-    try stdout.print("{!}\n", .{max});
+    const max: u64 = try solve(600851475143);
+    try stdout.print("{}\n", .{max});
 }
