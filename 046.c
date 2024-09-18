81b0c66dc8a84726d9cb8924ac90b94f98c56d75
lauchimoon <laurentino.luna06@gmail.com>
2024-09-17 23:55:44 -0300

    Solve 046 in C

--- 046.c
+++ 046.c
@@ -1,14 +1,13 @@
 // 5777
-#include <iostream>
-#include <vector>
-#include <cmath>
+#include <stdio.h>
+#include <math.h>
 
-#define EULER_COMMON_IMPL
-#include "common.hh"
+#define EULER_COMMON_IMPLEMENTATION
+#include "common.h"
 
-bool is_integer(float n)
+Bool is_integer(float n)
 {
-    int int_part = int(n);
+    int int_part = (int)n;
     float float_part = n - int_part;
     return float_part == 0.0f;
 }
@@ -17,16 +16,16 @@ int main()
 {
     int top = 5785;
     int x = 0;
-    bool indicator = true;
-    std::vector<bool> primes = eratosthenes(top);
+    Bool indicator = 1;
+    Bool *primes = eratosthenes(top);
     for (int i = 3; i < top; i += 2) {
-        indicator = false;
+        indicator = 0;
         if (!primes[i]) {
             for (int p = 2; p < i; ++p) {
                 if (primes[p]) {
                     float n = sqrt((i - p)/2);
                     if (is_integer(n) && i == p + 2*n*n) {
-                        indicator = true;
+                        indicator = 1;
                         break;
                     }
                 }
@@ -35,6 +34,7 @@ int main()
         }
     }
 
-    std::cout << x << "\n";
+    free(primes);
+    printf("%d\n", x);
     return 0;
 }
